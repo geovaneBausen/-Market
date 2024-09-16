9b7eed112133f6c8a49b42329476fc9b705131f4
geovaneBausen <168789898+geovaneBausen@users.noreply.github.com>
2024-09-16 20:26:30 -0300

    Add files via upload
    update

--- src/paginas/Sobre/index.js
+++ src/paginas/Sobre/index.js
@@ -1,110 +1,124 @@
-import { useState } from "react";
 import style from "./sobre.module.css";
+import { Icon } from "@iconify/react";
+import { Link } from 'react-router-dom';
 
 function Sobre() {
   return (
     <div className={style.container}>
       <div className={style.cardPrincipal}>
         <div className={style.sobrenos}>
-          <div>
-            <h2 className={style.subtitulo}>
-              DESENVOLVIMENTO DE UMA APLICAÇÃO WEB COM REACT
-            </h2>
+          <h2 className={style.subtitulo}>
+            DESENVOLVIMENTO DE UMA APLICAÇÃO WEB COM REACT
+          </h2>
 
-            {/* Seção "Funcionalidade" */}
-            <div className={style.secao}>
-              <h3 className={style.tituloSecao}>Funcionalidade</h3>
-              <p className={style.paragrafo}>
-                A aplicação web desenvolvida tem como objetivo simular uma <b>loja virtual</b>, oferecendo uma interface interativa com funcionalidades de <b>busca de produtos da api do mercado livre</b>, <b>visualização detalhada de itens</b>, <b>simulação de envio de um formulário</b> e <b>navegação intuitiva</b>. Todos esses recursos foram implementados utilizando a biblioteca React, garantindo que os usuários possam explorar os produtos de forma eficiente.
-              </p>
+          {/* Seção "Funcionalidade" */}
+          <div className={style.secao}>
+            <h3 className={style.tituloSecao}>Funcionalidade</h3>
+            <p className={style.paragrafo}>
+              A aplicação web desenvolvida tem como objetivo simular uma <b>loja virtual</b>, oferecendo uma interface interativa com funcionalidades de <b>busca de produtos através da api do mercado livre</b>, <b>visualização detalhada de itens</b>, <b>simulação de envio de um formulário</b> e <b>navegação intuitiva</b>. Todos esses recursos foram implementados utilizando a biblioteca React, garantindo que os usuários possam explorar os produtos de forma eficiente.
+            </p>
 
-              <div className={style.imagemContainer}>
-                <p className={style.imagemDescricao}>
-                  Como a API do Mercado Livre é utilizada para buscar e exibir produtos.
-                </p>
-                <img src="./imagens/apiQuery.png" alt="Logo" className={style.imagem} />
-                <img src="./imagens/apiid.png" alt="Logo" className={style.imagem} />
-
-              </div>
-
+            <p className={style.imagemDescricao}>
+              Como a API do Mercado Livre é utilizada para buscar e exibir produtos.
+            </p>
+            <div className={style.imagemContainer}>
+              <img src="./imagens/apiQuery.png" alt="API Query" className={style.imagem} />
+              <img src="./imagens/apiId.png" alt="API ID" className={style.imagem} />
             </div>
 
-            {/* Paginas*/}
-            <div className={style.secao}>
-              <h3 className={style.tituloSecao}>Paginas</h3>
-              <p className={style.paragrafo}>
-                <strong>Página Inicial:</strong> Lista de produtos do Mercado Livre. <br />
-                <strong>Página de Cadastro:</strong> Permite que usuários simulem um envio de formulário para criação de conta. <br />
-                <strong>Página Sobre:</strong> Informações sobre o site e sua proposta. <br />
-                <strong>Página de Item:</strong> Detalhes específicos de um produto. <br />
-                <strong>Página Não Encontrada:</strong> Mensagem exibida quando a página solicitada não existe.
-              </p>
+            <p className={style.paragrafo}>
+              <b>A função fetchProducts</b> faz uma requisição à API do Mercado Livre para buscar produtos baseados no termo de pesquisa (query), converte a resposta em JSON e retorna a lista de produtos encontrada. Ao ser chamada com um termo, por exemplo, fetchProducts("celular"), ela vai retornar os resultados da busca por "celular" no Mercado Livre.
+            </p>
+            <p className={style.paragrafo}>
+              Já a <b>função fetchItem</b> faz uma requisição à API do Mercado Livre para obter informações detalhadas sobre um item específico, usando o itemId fornecido. Converte a resposta em JSON e retorna os dados do item. Ao ser chamada com um ID de item, por exemplo, fetchItem("MLB123456"), a função retornará os detalhes do item com o ID "MLB123456" no Mercado Livre.
+            </p>
+          </div>
 
-              <div className={style.imagemContainer}>
-                <p className={style.imagemDescricao}>
-                  A aplicação foi estruturada com base em componentes reutilizáveis, organizados em pastas e conectados através do React Router.
-                </p>
-                <img src="./imagens/rotas.png" alt="Logo" className={style.imagem} />
+          {/* Seção "Paginas" */}
+          <div className={style.secao}>
+            <h3 className={style.tituloSecao}>Paginas</h3>
+            <p className={style.paragrafo}>
+              <strong>Página Inicial:</strong> Lista de produtos do Mercado Livre. <br />
+              <strong>Página de Cadastro:</strong> Permite que usuários simulem um envio de formulário para criação de conta. <br />
+              <strong>Página Sobre:</strong> Informações sobre o site e sua proposta. <br />
+              <strong>Página de Item:</strong> Detalhes específicos de um produto. <br />
+              <strong>Página Não Encontrada:</strong> Mensagem exibida quando a página solicitada não existe.
+            </p>
 
-              </div>
-
+            <p className={style.imagemDescricao}>
+              A aplicação foi estruturada com base em componentes reutilizáveis, organizados em pastas e conectados através do React Router.
+            </p>
+            <div className={style.imagemContainer}>
+              <img src="./imagens/rotas.png" alt="Rotas" className={style.imagem} />
             </div>
+          </div>
 
-            {/* Seção "Componentização" */}
-            <div className={style.secao}>
-              <h3 className={style.tituloSecao}>Componentização</h3>
-              <p className={style.paragrafo}>
-                O projeto foi estruturado utilizando componentes reutilizáveis, facilitando a manutenção e expansão da aplicação.
-              </p>
+          {/* Seção "Componentização" */}
+          <div className={style.secao}>
+            <h3 className={style.tituloSecao}>Componentização</h3>
+            <p className={style.paragrafo}>
+              O projeto foi estruturado utilizando componentes reutilizáveis, facilitando a manutenção e expansão da aplicação.
+            </p>
 
-              <div className={style.imagemContainer}>
-
-                <img src="./imagens/componentes.png" alt="Logo" className={style.imagem} />
-
-              </div>
+            <div className={style.imagemContainer}>
+              <img src="./imagens/componentes.png" alt="Componentes" className={style.imagem} />
             </div>
+          </div>
 
-            {/* Seção "Estilização e Design" */}
-            <div className={style.secao}>
-              <h3 className={style.tituloSecao}>Estilização e Design</h3>
-              <p className={style.paragrafo}>
-                A aplicação foi estilizada com CSS Modules, priorizando a responsividade e uma experiência visual moderna. A modularização do CSS garante o escopo local dos estilos, evitando conflitos de nomenclatura e promovendo um desenvolvimento mais organizado. Boas práticas de design foram aplicadas para garantir uma interface intuitiva e funcional em diferentes tamanhos de tela, desde desktops até dispositivos móveis.
-              </p>
-              <div className={style.imagemContainer}>
-
-                <img src="./imagens/componentes.png" alt="Logo" className={style.imagem} />
-
-              </div>
+          {/* Seção "Estilização e Design" */}
+          <div className={style.secao}>
+            <h3 className={style.tituloSecao}>Estilização e Design</h3>
+            <p className={style.paragrafo}>
+              A aplicação foi estilizada com CSS Modules, priorizando a responsividade e uma experiência visual moderna. A modularização do CSS garante o escopo local dos estilos, evitando conflitos de nomenclatura e promovendo um desenvolvimento mais organizado. Boas práticas de design foram aplicadas para garantir uma interface intuitiva e funcional em diferentes tamanhos de tela, desde desktops até dispositivos móveis.
+            </p>
+            <div className={style.imagemContainer}>
+              <img src="./imagens/iphone14ProMax.png" alt="Iphone 14 ProMax" className={style.imagem} />
+              <p className={style.imagemDescricao}>Ipad Mini</p>
+              <img src="./imagens/ipadMini.png" alt="Ipad Mini" className={style.imagem} />
+              <p className={style.imagemDescricao}>Iphone 14 ProMax</p>
             </div>
+          </div>
 
-            {/* Seção "Gerenciamento de Estado" */}
-            <div className={style.secao}>
-              <h3 className={style.tituloSecao}>Gerenciamento de Estado</h3>
-              <p className={style.paragrafo}>
-                O gerenciamento de estado foi feito utilizando o hook <b>useState</b> do React, permitindo que a interface responda dinamicamente às ações dos usuários. Arrays de objetos, representando os produtos, são manipulados e atualizados conforme o usuário interage com a aplicação. Esse gerenciamento eficiente do estado garante que as informações exibidas estejam sempre atualizadas.
-              </p>
-            </div>
+          {/* Seção "Hooks e Efeitos" */}
+          <div className={style.secao}>
+            <h3 className={style.tituloSecao}>Hooks e Efeitos</h3>
+            <p className={style.paragrafo}>
+              O hook <b>useState</b> é amplamente utilizado para gerenciar o estado local em vários componentes como:
+              <ul className={style.listaSemBolinha}>
+                <li><b>Busca</b>: Armazena o termo de busca digitado pelo usuário.</li>
+                <li><b>Contador</b>: Mantém o valor do contador e atualiza-o a cada clique no botão.</li>
+                <li><b>Formulário</b>: Gerencia os dados do formulário e a lista de usuários submetidos.</li>
+                <li><b>Cabeçalho</b>: Controla a visibilidade do menu de navegação.</li>
+                <li><b>Listagem de Itens</b>: Armazena itens recebidos da API, itens filtrados e controla o estado de carregamento.</li>
+              </ul>
+              O hook <b>useEffect</b> é utilizado para:
+              <ul className={style.listaSemBolinha}>
+                <li><b>Formulário</b>: Exibir a tabela de usuários no console após cada atualização da lista de usuários.</li>
+                <li><b>Listagem de Itens</b>: Buscar itens da API quando o componente é montado e atualizar os estados com os dados recebidos.</li>
+              </ul>
+              Isso garante que a interface se atualize conforme necessário e mantenha uma experiência de usuário interativa e eficiente.
+            </p>
+          </div>
 
-            {/* Seção "Publicação" */}
-            <div className={style.secao}>
-              <h3 className={style.tituloSecao}>Publicação</h3>
-              <p className={style.paragrafo}>
-                A aplicação foi publicada na plataforma Vercel, permitindo acessibilidade online para testes e demonstração das funcionalidades. O link para acesso ao projeto está disponível abaixo:
-              </p>
-              <a href="https://api.mercadolibre.com/sites/MLB/search?q=livro">
-                https://api.mercadolibre.com/sites/MLB/search?q=livro
-              </a>
-            </div>
+          {/* Seção "Publicação" */}
+          <div className={style.secao}>
+            <h3 className={style.tituloSecao}>Publicação</h3>
+            <p className={style.paragrafo}>
+              A aplicação foi publicada na plataforma Vercel, permitindo acessibilidade online para testes e demonstração das funcionalidades. O link para acesso ao projeto está disponível abaixo:
+            </p>
+            <a href="" className={style.link}>Link para o projeto</a>
+          </div>
 
-            {/* Seção "Documentação" */}
-            <div className={style.secao}>
-              <h3 className={style.tituloSecao}>Documentação</h3>
-              <p className={style.paragrafo}>
-                O projeto está devidamente documentado no GitHub, com instruções claras para instalação e execução da aplicação. Além disso, o código-fonte está bem organizado, com comentários explicativos e arquivos bem estruturados, garantindo a facilidade de compreensão e manutenção.
-              </p>
-              <p className={style.paragrafo}>
-                Data de Entrega: a definir. Formato de Entrega: Repositório no GitHub com o código da aplicação, instruções de execução, link para a aplicação publicada e o arquivo .zip do source enviado via AVA.
-              </p>
+          {/* Seção "Documentação" */}
+          <div className={style.secao}>
+            <h3 className={style.tituloSecao}>Documentação</h3>
+            <p className={style.paragrafo}>
+              O projeto está no GitHub, com instruções para instalação e execução da aplicação.
+            </p>
+            <div className={style.linkContainer}>
+              <Link to="https://github.com/geovaneBausen" target="_blank" rel="noopener noreferrer">
+                <Icon icon="fe:github" className={style.githubIcon} />
+              </Link>
             </div>
           </div>
         </div>
